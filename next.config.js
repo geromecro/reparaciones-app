4c63bf5916905b0998ecb89a28a5f6a5d98918bb
Geronimo Melqui <geronimo@Geronimos-MacBook-Air.local>
2025-11-10 11:25:36 -0300

    Force clean rebuild - disable ETag generation for cache busting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Minimalista design system v2.0 - Cache busting
+  generateEtags: false,
 };
 
 module.exports = nextConfig;
